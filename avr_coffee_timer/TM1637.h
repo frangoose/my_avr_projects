e52c9f2980f3c516e9308d51d1fc68a1d2a38a6d
frangoose <frangoose@gmail.com>
2021-08-15 22:55:54 -0700

    Added Block Diagram for avr_coffee_roaster

---
+++ avr_coffee_timer/TM1637.h
@@ -0,0 +1,49 @@
+/* FILENAME     TM1637.h
+ * TARGET       ATMEL ATMEGA328P
+ * DATE CREATED 5/14/2020
+ * AUTHOR       NICK FRANGOS
+ */
+
+#ifndef TM1637_4DIG_DISPLAY_H
+#define TM1637_4DIG_DISPLAY_H
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#define TM_DELAY 5 // microseconds
+
+// IO Macros
+#define TM_DDR  DDRD
+#define TM_PORT PORTD
+#define TM_CLK  2
+#define TM_DIO  3
+
+// Command Macros
+#define TM_AUTOADDR 0x40
+#define TM_FIXADDR  0x44
+#define TM_GRID1    0xC0
+#define TM_GRID2    0xC1
+#define TM_GRID3    0xC2
+#define TM_GRID4    0xC3
+#define TM_GRID5    0xC4
+#define TM_GRID6    0xC5
+#define TM_DISPOFF  0x80
+#define TM_DISPON   0x8F
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void TM_init(void);
+void TM_start(void);
+void TM_stop(void);
+void TM_send(uint8_t byte);
+void TM_diolow();
+void TM_diohigh();
+void TM_clklow();
+void TM_clkhigh();
+void TM_fillDisplay();
+void TM_clearDisplay();
+void TM_printValue(uint8_t grid1, uint8_t grid2, uint8_t grid3, uint8_t grid4);
+
+#endif
--- avr_coffee_timer/main.c
+++ avr_coffee_timer/main.c
@@ -1,21 +1,92 @@
-/* PROJECT NAME AVR_COFFEE_TIMER 
+/* PROJECT NAME AVR_COFFEE_TIMER
  * TARGET       ATMEL ATMEGA328P
- * DATE CREATED 7/24/2020
+ * DATE CREATED 8/12/2020
  * AUTHOR       NICK FRANGOS
  */
 
 #define F_CPU 16000000UL
-#define BLINK_DELAY 1000
 
+#include <stdlib.h>
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "TM1637.h"
+
+extern uint8_t segmap[];
+
+uint16_t ms = 0;
+uint8_t sec = 0;
+uint8_t min = 0;
+uint8_t start_flag = 0;
+
+ISR(TIMER1_COMPA_vect)
+{
+//  count 999 times in ms counter
+    if(start_flag == 0)
+    {
+        ms = 0;
+    }
+    else
+    {
+        if(ms < 999)
+        {
+            ms++; 
+        }
+    //  count 1000th time and reset ms counter
+        else
+        {
+            ms = 0;
+            sec++;
+            if(sec > 59)
+            {
+                sec=0;
+                min++;
+            }
+            if(min > 99)
+            {
+                min=0;
+                //stop timer
+            }
+        }
+    }
+}
 
 int main(void)
 {
-    DDRB |= (1<<DDB5);
+    char min_buffer[2] = {0};
+    char sec_buffer[2] = {0};
+
+    uint8_t b0_state = 0;
+    uint8_t b01_state = 0;
+
+    min = (min_buffer[0] - '0') * 10;
+    min = min + (min_buffer[1] - '0');
+    sec = (sec_buffer[0] - '0') * 10;
+    sec = sec + (sec_buffer[1] - '0');
+    
+    TM_init();
+    TM_start();
+    TM_send(TM_DISPON);
+    TM_stop();
+    TM_fillDisplay();
+    _delay_ms(500);
+    TM_clearDisplay();
+    TM_printValue(segmap[(min/10)],segmap[(min%10)],segmap[(sec/10)],segmap[(sec%10)]);
+    
+    TCCR1B |= (1 << CS10) | (1<<WGM12); // set prescalar to 1
+    OCR1A  = 16000; // match 1ms at 16MHZ FCPU
+    TIMSK1 |= (1 << OCIE1A);
+    sei();
+    
     while(1)
     {
-
+        if(sec & 0x01)
+        {
+            TM_printValue(segmap[(min/10)],segmap[(min%10)]+0x80,segmap[(sec/10)],segmap[(sec%10)]);
+        }
+        else
+        {
+            TM_printValue(segmap[(min/10)],segmap[(min%10)],segmap[(sec/10)],segmap[(sec%10)]);
+        }
     }
     return 0;
 }
